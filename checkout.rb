0ce04a639e26ea9c468732c400a1c17cbb98de78
David BF <burretfresket@gmail.com>
2018-12-13 07:37:07 +0100

    Add Checkout unit tests

--- checkout.rb
+++ checkout.rb
@@ -1,2 +1,24 @@
 class Checkout
+  def initialize(pricing_rules)
+    @pricing_rules = pricing_rules
+    @items = []
+  end
+
+  def scan(item)
+    @items << item
+  end
+
+  def total
+    pricing_rules.total_for(items)
+  end
+
+  private
+
+  def pricing_rules
+    @pricing_rules
+  end
+
+  def items
+    @items
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,6 +3,8 @@ require './checkout'
 RSpec.describe Checkout do
   describe 'Integration' do
     it 'computes the total' do
+      pricing_rules = nil
+      green_tea = nil
       co = Checkout.new(pricing_rules)
       co.scan(green_tea)
       price = co.total
@@ -10,10 +12,30 @@ RSpec.describe Checkout do
       expect(price).to eq('£3.11')
     end
   end
-end
 
-def pricing_rules
-end
+  describe '#total' do
+    it 'delegates to pricing rules' do
+      pricing_rules = double(:pricing_rules)
+      allow(pricing_rules).to receive(:total_for)
 
-def green_tea
+      co = described_class.new(pricing_rules)
+      co.total
+
+      expect(pricing_rules).to have_received(:total_for).with([])
+    end
+
+    it 'sends the scanned items' do
+      pricing_rules = double(:pricing_rules)
+      allow(pricing_rules).to receive(:total_for)
+      items = ['Green Tea', 'Coffee']
+
+      co = described_class.new(pricing_rules)
+      items.each do |item|
+        co.scan(item)
+      end
+      co.total
+
+      expect(pricing_rules).to have_received(:total_for).with(items)
+    end
+  end
 end
